39207ba347e6ce78ec652112456855a43c887f7e
Michael Lee <mlee42@cs.washington.edu>
2017-04-18 20:01:16 -0700

    Refine bag of words model to report more statistics

--- abuse/models/bag_of_words.py
+++ abuse/models/bag_of_words.py
@@ -1,7 +1,7 @@
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
+import sklearn.metrics
 
 from custom_types import *
 from parsing import load_raw_data
@@ -16,17 +16,39 @@ def bag_of_words_model(train: TrainData, dev: DevData, test: TestData) -> None:
     train_comment = [c.comment for c in train]
     train_attack = [c.average.attack > 0.5 for c in train]
 
-    test_comment = [c.comment for c in test]
-    test_attack = [c.average.attack > 0.5 for c in test]
+    dev_comment = [c.comment for c in dev]
+    dev_attack = [c.average.attack > 0.5 for c in dev]
 
     clf.fit(train_comment, train_attack)
 
-    auc = roc_auc_score(test_attack, clf.predict_proba(test_comment)[:, 1])
+    # Evaluation
+    dev_attack_predicted = clf.predict(dev_comment)
 
-    print('Test ROC AUC: {}'.format(auc))
+    print("    Accuracy:   {:.6f}".format(sklearn.metrics.accuracy_score(dev_attack, dev_attack_predicted)))
+    print("    Precision:  {:.6f}".format(sklearn.metrics.precision_score(dev_attack, dev_attack_predicted)))
+    print("    Recall:     {:.6f}".format(sklearn.metrics.recall_score(dev_attack, dev_attack_predicted)))
+    print("    F1 score:   {:.6f}".format(sklearn.metrics.f1_score(dev_attack, dev_attack_predicted)))
+    print("    AUC score:  {}".format(sklearn.metrics.roc_auc_score(dev_attack, dev_attack_predicted)))
+    print("    Confusion matrix:")
+    print(sklearn.metrics.confusion_matrix(dev_attack, dev_attack_predicted))
+    print()
+
+    count = 0
+    for idx, (actual, predicted) in enumerate(zip(dev_attack, dev_attack_predicted)):
+        if actual != predicted:
+            print(dev[idx].comment)
+            print('Attack score={}, is_attack={}, predicted={}'.format(dev[idx].average.attack, actual, predicted))
+            print()
+            count += 1
+
+        if count == 30:
+            break
 
 
 if __name__ == '__main__':
-    train, dev, test = load_raw_data()
+    data_src_path = 'data/wikipedia-detox-data-v6'
+    print("Loading...")
+    train, dev, test = load_raw_data(data_src_path)
+    print("Training...")
     bag_of_words_model(train, dev, test)
 
