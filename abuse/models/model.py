f430e17dd8a38149dc9c15eb6d46383b5a71400e
Michael Lee <mlee42@cs.washington.edu>
2017-04-21 23:09:57 -0700

    Refine metrics output; make models.py typecheck

--- abuse/models/model.py
+++ abuse/models/model.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Any, Generic, TypeVar, Iterable
+from typing import Dict, List, Any, Generic, TypeVar, Iterable 
 
 import sklearn.metrics as metrics
 
@@ -6,11 +6,10 @@ import utils.file_manip as fmanip
 
 TSelf = TypeVar('TSelf', bound='Model')
 TInput = TypeVar('TInput')
-TLabel = TypeVar('TLabel')
 
-class ClassificationMetrics(Generic[TLabel]):
-    def __init__(self, y_expected: List[TLabel], 
-                       y_predicted: List[TLabel]) -> None:
+class ClassificationMetrics:
+    def __init__(self, y_expected: List[List[bool]], 
+                       y_predicted: List[List[bool]]) -> None:
         self.accuracy = metrics.accuracy_score(y_expected, y_predicted)
         self.precision = metrics.precision_score(y_expected, y_predicted)
         self.recall = metrics.recall_score(y_expected, y_predicted)
@@ -29,39 +28,19 @@ class ClassificationMetrics(Generic[TLabel]):
 
     def get_header(self) -> str:
         return ("| Accuracy | Precision | Recall | F1 | ROC |\n" +
-                "| -------- | --------- | ------ | -- | --- |\n")
+                "| -------- | --------- | ------ | -- | --- |")
 
 
-class Model(Generic[TInput, TLabel]):
+class Model(Generic[TInput]):
     # Core methods that must be implemented
-    def __init__(self) -> None:
+    def __init__(self, **params) -> None:
         '''For the sake of consistency, the constructor for all subclasses
-        should take in no params and only set default values for parameters.'''
+        should take in an arbitrary set of parameters, and do very little
+        else. All parameters should have a default value.'''
         raise NotImplementedError()
 
-    def build_model(self) -> None:
-        '''Builds and saves the model, using the currently set params.'''
+    def get_parameters(self) -> Dict[str, Any]:
         raise NotImplementedError()
-
-    def reset(self) -> None:
-        '''Resets the model, clearing all training data/any learned information'''
-        raise NotImplementedError()
-
-    def train(self, xs: List[TInput], ys: List[TLabel], **params) -> None:
-        '''Trains the model. The expectation is that this method is called
-        exactly once.'''
-        raise NotImplementedError()
-
-    def train_iterative(self, xs: List[TInput], ys: List[TLabel], **params) -> Iterable[List[TLabel]]:
-        '''Also trains the model, but should yield after each epoch so the client
-        can analyze data/collect statistics.'''
-        raise NotImplementedError()
-
-    def predict_single(self, xs: TInput) -> TLabel:
-        raise NotImplementedError()
-
-    def predict(self, xs: List[TInput]) -> List[TLabel]:
-        return [self.evaluate_single(x) for x in xs]
 
     def _save_model(self, path: str) -> None:
         raise NotImplementedError()
@@ -69,30 +48,36 @@ class Model(Generic[TInput, TLabel]):
     def _restore_model(self, path: str) -> None:
         raise NotImplementedError()
 
-    def get_parameters(self) -> Dict[str, Any]:
+    def build_model(self) -> None:
+        '''Builds and saves the model, using the currently set params.'''
         raise NotImplementedError()
 
-    def set_parameters(self, params: Dict[str, Any]) -> None:
+    def train(self, xs: List[TInput], ys: List[List[bool]]) -> None:
+        '''Trains the model. The expectation is that this method is called
+        exactly once.'''
+        raise NotImplementedError()
+
+    def predict(self, xs: List[TInput]) -> List[List[bool]]:
         raise NotImplementedError()
 
     # Useful utility methods
+    def predict_single(self, x: TInput) -> List[bool]:
+        return self.predict([x])[0]
 
     @classmethod
-    def restore_from_saved(cls, path: str) -> None:
-        self.set_parameters(fmanip.load_json(fmanip.join(path, 'params.json')))
-        self._restore_model(fmanip.join(path, 'model'))
-
-    @classmethod
-    def make_new(cls: TSelf, **params: Any) -> TSelf:
-        model = cls()
-        model.set_parameters(params)
-        model.build_model()
-        return model
+    def restore_from_saved(cls: Any, path: str) -> Any:
+        # Signature really should be
+        # (Type[TSelf], str) -> TSelf
+        # ...but idk if mypy supports this fully atm
+        obj = cls(**fmanip.load_json(fmanip.join(path, 'params.json')))
+        obj._restore_model(fmanip.join(path, 'model'))
+        return obj
 
     def save(self, path: str) -> None:
         fmanip.ensure_folder_exists(path)
         param_path = fmanip.join(path, 'params.json')
-        model_path = fmanip.ensure_folder_exists(fmanip.join(path, 'model'))
+        model_path = fmanip.join(path, 'model')
+        fmanip.ensure_folder_exists(model_path)
 
         fmanip.write_nice_json(self.get_parameters(), param_path)
         self._save_model(model_path)
