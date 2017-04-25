c4f458c811e121c00d78b9cdb573800bc20a42e2
iluvredwall <bobomb.asplode@gmail.com>
2017-04-25 01:42:26 -0700

    Refactor logistic classifier to follow model interface

--- abuse/cmd2.py
+++ abuse/cmd2.py
@@ -12,6 +12,7 @@ import numpy as np  # type: ignore
 
 from data_extraction.wikipedia import *
 from models.bag_of_words import BagOfWordsClassifier
+from models.logistic_classifier import LogisticClassifier
 from models.rnn_classifier import RnnClassifier
 from models.rnn_char_classifier import RnnCharClassifier
 from models.logistic_copy import CopiedClassifier
@@ -82,6 +83,7 @@ def main() -> None:
 
     models = {
             'bag_of_words': BagOfWordsClassifier,
+            'lr': LogisticClassifier,
             'copy': CopiedClassifier,
             'rnn': RnnClassifier,
             'rnn_char': RnnCharClassifier,
--- abuse/models/logistic_classifier.py
+++ abuse/models/logistic_classifier.py
@@ -1,119 +1,170 @@
+from typing import List, Tuple, Optional, Dict, Generator, TypeVar
 import os
 import glob
 import random
 from math import ceil
-import numpy as np
-from scipy.sparse import csr_matrix
-import tensorflow as tf
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import recall_score, precision_score, f1_score, roc_auc_score, roc_curve
-from scipy.stats import spearmanr
-import matplotlib.pyplot as plt
+import numpy as np  # type: ignore
+from scipy.sparse import csr_matrix  # type: ignore
+import tensorflow as tf  # type: ignore
+from sklearn.feature_extraction.text import TfidfVectorizer  # type: ignore
+from sklearn.metrics import recall_score, precision_score, f1_score, roc_auc_score, roc_curve  # type: ignore
+from scipy.stats import spearmanr  # type: ignore
+
+from custom_types import *
+from models.model import Model
 
 # default path to save things in
 SUMMARY_PATH = "runs/lr{0}"
 
-def chunks(l, n):
+T = TypeVar("T")
+
+def chunks(l: List[T], n: int) -> Generator[List[T], None, None]:
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-class LogisticClassifier(object):
-    def __init__(self, num_grams, base_summary_path=SUMMARY_PATH):
+class LogisticClassifier(Model[str]):
+    def __init__(self,
+                 num_grams: int=30000,
+                 batch_size: int=32,
+                 epochs: int=20,
+                 pos_weight: float=10,
+                 reg_lambda: float=1e-7,
+                 learning_rate: float=0.0001,
+                 beta1: float=0.9,
+                 beta2: float=0.999,
+                 epsilon: float=1e-8,
+                 base_summary_path: str=SUMMARY_PATH,
+                 restore_from: Optional[str]=None) -> None:
         self.num_grams = num_grams
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.pos_weight = pos_weight
+        self.reg_lambda = reg_lambda
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
         self.base_summary_path = base_summary_path
 
         self.config = tf.ConfigProto(allow_soft_placement=True)
         self.graph = tf.Graph()
+
+        self._build()
         self.reset()
 
+        if restore_from is not None:
+            self._restore_model(restore_from)
+
+
+    def reset(self) -> None:
         with self.graph.as_default():
             with self.graph.device("/gpu:0"):
-                self.counts = tf.sparse_placeholder(tf.float32, [None, self.num_grams], "counts")
-                self.target_labels = tf.placeholder(tf.bool, [None], "target_labels")
-
-                # self.pred_labels
-                self.logits, self.probabilities, self.regularizer = self.build_model(self.counts)
-
-                # I'm pretty sure this is necessary to add things to the summary
-                self.loss = self.calculate_loss(self.logits, self.target_labels, self.regularizer)
-
-                self.train_op = None
-
-                self.summary = tf.summary.merge_all()
-
-                self.saver = tf.train.Saver()
-
-
-    def reset(self):
-        self.session = tf.Session(graph=self.graph, config=self.config)
+                self.session = tf.Session(graph=self.graph, config=self.config)
+                self.session.run(self.init)
+        
+        self.summary_path, self.run_num = self._get_next_summary_path()
+        self.it = 0
+        self.epoch = 0
+    
+    def _get_next_summary_path(self) -> Tuple[str, int]:
         i = 0
         while True:
             i += 1
             summary_path = self.base_summary_path.format(i)
             if not os.path.exists(summary_path) and not glob.glob(summary_path + "-*"):
-                break
-        self.summary_path = summary_path
-        self.run_num = i
-        self.it = 0
-        self.epoch = 0
+                return summary_path, i
+    
+    def get_parameters(self) -> Dict[str, Any]:
+        return {
+                "num_grams": self.num_grams,
+                "batch_size": self.batch_size,
+                "epochs": self.epochs,
+                "pos_weight": self.pos_weight,
+                "reg_lambda": self.reg_lambda,
+                "learning_rate": self.learning_rate,
+                "beta1": self.beta1,
+                "beta2": self.beta2,
+                "epsilon": self.epsilon,
+                "base_summary_path": self.base_summary_path,
+                "summary_path": self.summary_path,
+                "run_num": self.run_num,
+                "it": self.it,
+                "epoch": self.epoch
+        }
 
-    def build_model(self, counts):
+    def _build(self) -> None:
+        with self.graph.as_default():
+            with self.graph.device("/gpu:0"):
+                self._build_inputs()
+                self._build_model()
+                self._build_loss()
+                self._build_optimizer()
+                self.init = tf.global_variables_initializer()
+                self.summary = tf.summary.merge_all()
+                self.saver = tf.train.Saver()
+
+    def _build_inputs(self) -> None:
+        self.tfidf_vectorizer = None  # type: ignore
+        self.counts = tf.sparse_placeholder(tf.float32, [None, self.num_grams], "counts")
+        self.target_labels = tf.placeholder(tf.bool, [None], "target_labels")
+    
+    def _build_model(self) -> None:
         with tf.name_scope("model"):
             W = tf.Variable(tf.random_normal([self.num_grams, 1]), name="weight")
             b = tf.Variable(tf.zeros([1]), name="bias")
-            matmul = tf.squeeze(tf.sparse_tensor_dense_matmul(counts, W), axis=1)
-            logits = tf.add(matmul, b)
+            matmul = tf.squeeze(tf.sparse_tensor_dense_matmul(self.counts, W), axis=1)
+            self.logits = tf.add(matmul, b)
+
         with tf.name_scope("loss"):
-            regularizer = tf.nn.l2_loss(W)
+            self.regularizer = tf.nn.l2_loss(W, name="regularizer")
 
-        with tf.name_scope("prediction"):
-            probabilities = tf.sigmoid(logits)
-            # labels = tf.round(probabilities)
+        with tf.name_scope("probabilities"):
+            self.probabilities = tf.sigmoid(self.logits)
 
-        return logits, probabilities, regularizer
+    def _build_loss(self) -> None:
+        with tf.name_scope("loss"):
+            cross_entropy = tf.nn.weighted_cross_entropy_with_logits(
+                logits=self.logits, targets=tf.to_float(self.target_labels), pos_weight=self.pos_weight)
+            cross_entropy_mean = tf.reduce_mean(cross_entropy, name="x_entropy_mean")
+            self.loss = tf.reduce_mean(cross_entropy_mean + self.reg_lambda * self.regularizer)
 
-    def calculate_loss(self, logits, labels, regularizer, reg_lambda=1e-7, pos_weight=1):
-        with self.graph.as_default():
-            with self.graph.device("/gpu:0"):
-                with tf.name_scope("loss"):
-                    cross_entropy = tf.nn.weighted_cross_entropy_with_logits(
-                        logits=logits, targets=tf.to_float(labels), pos_weight=pos_weight)
-                    cross_entropy_mean = tf.reduce_mean(cross_entropy, name="x_entropy_mean")
+            tf.summary.scalar("x_entropy_mean", cross_entropy_mean)
+            tf.summary.scalar("loss", self.loss)
 
-                    loss = tf.reduce_mean(cross_entropy_mean + reg_lambda * regularizer)
+    def _build_optimizer(self) -> None:
+        with tf.name_scope("optimizer"):
+            self.optimizer = tf.train.AdamOptimizer(
+                learning_rate=self.learning_rate,
+                beta1=self.beta1,
+                beta2=self.beta2,
+                epsilon=self.epsilon).minimize(self.loss)
+    
+    def train(self, comments: List[str], labels: List[int], epochs: Optional[int]=None, batch_size: Optional[int]=None, **params: Any) -> None:
+        if self.tfidf_vectorizer is None:
+            self.tfidf_vectorizer = TfidfVectorizer(
+                norm="l2",
+                ngram_range=(1,5),
+                max_features=self.num_grams,
+                dtype=np.float32)  # lowercase = False?
+            self.tfidf_vectorizer.fit(comments)  # type: ignore
+        counts = self.tfidf_vectorizer.transform(comments)  # type: ignore
+        labels = np.array(labels, dtype=np.bool)  # type: ignore
+        self._train(counts, labels, epochs, batch_size)  # type: ignore
 
-                    tf.summary.scalar("x_entropy_mean", cross_entropy_mean)
-                    tf.summary.scalar("loss", loss)
-                    return loss
-
-    def make_train_op(self, loss, rate, epsilon, initialize=True):
-        with self.graph.as_default():
-            with self.graph.device("/gpu:0"):
-                with tf.name_scope("train"):
-                    optimizer = tf.train.AdamOptimizer(learning_rate=rate, epsilon=epsilon)
-                    op = optimizer.minimize(loss)
-
-                    if initialize:
-                        self.session.run(tf.global_variables_initializer())
-
-                    return op
-
-    def train(self, counts, labels, epochs=1, batch_size=32, rate=0.0001, epsilon=1e-8, reg_lambda=1e-7, pos_weight=10, reset=True):
-        if reset:
-            self.reset()
-            loss = self.calculate_loss(self.logits, self.target_labels, self.regularizer, reg_lambda, pos_weight)
-            # self.loss = loss
-            self.train_op = self.make_train_op(loss, rate, epsilon, initialize=reset)
+    def _train(self, counts: List[List[float]], labels: List[bool], epochs: Optional[int]=None, batch_size: Optional[int]=None) -> None:
+        if epochs is None:
+            epochs = self.epochs
+        if batch_size is None:
+            batch_size = self.batch_size
 
         batch_count = ceil(len(labels) / batch_size)
         indices = list(range(len(labels)))
         writer = tf.summary.FileWriter(self.summary_path, self.graph)
 
-
         for _ in range(epochs):
             self.epoch += 1
-            print("=== EPOCH", self.epoch, "===")
+            print("=== EPOCH {} ===".format(self.epoch))
 
             random.shuffle(indices)
             batches = chunks(indices, batch_size)
@@ -124,43 +175,62 @@ class LogisticClassifier(object):
                 i += 1
                 if i % 10 == 0:
                     print("Training batch", i, "of", batch_count, end="\r")
-                batch_counts = csr_to_stv(counts[batch])
-                summary, _ = self.session.run([self.summary, self.train_op],
+                batch_counts = csr_to_stv(counts[batch])  # type: ignore
+                summary, _ = self.session.run([self.summary, self.optimizer],
                                               {self.counts: batch_counts,
-                                               self.target_labels: labels[batch]})
+                                               self.target_labels: labels[batch]})  # type: ignore
                 writer.add_summary(summary, self.it)
             # clear current line for any reasonable batch count
             print("                                      ", end="\r")
         writer.close()
 
-    def test(self, counts, labels):
-        probs = np.empty_like(labels, dtype=np.float32)
-        for i, (count, label) in enumerate(zip(counts, labels)):  # range(len(labels))
+    def predict(self, comments: List[str]) -> List[List[float]]:
+        counts = self.tfidf_vectorizer.transform(comments)  # type: ignore
+        return self._predict(counts)
+
+    def _predict(self, counts: List[List[float]]) -> List[List[float]]:
+        num_comments = counts.shape[0]  # type: ignore
+        probs = np.empty(num_comments, dtype=np.float32)
+        for i, count in enumerate(counts):
             if i % 500 == 0:
-                print("Testing number", i, "of", len(labels), end="\r")
+                print("Testing number", i, "of", num_comments, end="\r")
             probs[i] = self.session.run(self.probabilities,
-                                        {self.counts: csr_to_stv(count),  # counts[index]
-                                         self.target_labels: [label]})[0]  # labels[index]
+                                        {self.counts: csr_to_stv(count)})[0]
         print("                                      ", end="\r")
-        pred = probs.round()
+        return np.array([1-probs, probs]).T  # type: ignore
+    
+    def _test(self, counts: List[List[float]], labels: List[bool]) -> List[List[float]]:
+        probs = self._predict(counts)
+        pred = probs.argmax(axis=0)  # type: ignore
         evaluate(labels, pred)
         return probs
 
-    def train_test(self, train_counts, train_labels, test_counts, test_labels, epochs=1, reset=True, *args, **kwargs):
-        self.train(train_counts, train_labels, *args, **kwargs, reset=reset)
+    def _train_test(self, train_counts: List[List[float]], train_labels: List[bool], test_counts: List[List[float]], test_labels: List[bool],
+                   epochs: Optional[int]=None, batch_size: Optional[int]=None, test_interval: int=2) -> List[List[List[float]]]:
+        if epochs is None:
+            epochs = self.epochs
+        self._train(train_counts, train_labels, epochs=1, batch_size=batch_size)
         preds = []
-        preds.append(self.test(test_counts, test_labels))
-        for _ in range(1, epochs):
-            self.train(train_counts, train_labels, *args, **kwargs, reset=False)
-            preds.append(self.test(test_counts, test_labels))
+        if test_interval == 1:
+            preds.append(self._test(test_counts, test_labels))
+        for i in range(1, epochs):
+            self._train(train_counts, train_labels, epochs=1, batch_size=batch_size)
+            if (i+1) % test_interval == 0:
+                preds.append(self._test(test_counts, test_labels))
         return preds
 
-    def save(self, filename="model.ckpt", pathname=None):
+    def _save_model(self, path: str) -> None:
+        self.save(pathname=path)
+
+    def save(self, filename: str="model.ckpt", pathname: Optional[str]=None) -> None:
         if pathname is None:
             pathname = self.summary_path
-        self.saver.save(self.session, os.path.join(pathname, filename))
+        self.saver.save(self.session, os.path.join(pathname, filename))  # type: ignore
 
-    def restore(self, run_num=None, filename="model.ckpt", pathname=None):
+    def _restore_model(self, path: str) -> None:
+        self.restore(pathname=path)
+
+    def restore(self, run_num: Optional[int]=None, filename: str="model.ckpt", pathname: Optional[str]=None) -> None:
         # doesn't restore summary_path, run_num, it count, or epoch
         if pathname is None:
             if run_num is None:
@@ -177,23 +247,23 @@ class LogisticClassifier(object):
 
         with self.graph.as_default():
             with self.graph.device("/gpu:0"):
-                self.saver.restore(self.session, os.path.join(pathname, filename))
+                self.saver.restore(self.session, os.path.join(pathname, filename))  # type: ignore
 
-def evaluate(labels, pred):
+def evaluate(labels: List[bool], pred: List[bool]) -> None:
     print("AUC:", roc_auc_score(labels, pred))
     print("Spearman:", spearmanr(labels, pred).correlation)
     print("Precision:", precision_score(labels, pred))
     print("Recall:", recall_score(labels, pred))
     print("F1:", f1_score(labels, pred))
 
-def csr_to_stv(csr):
-    indices = np.array(csr.nonzero()).T
-    return tf.SparseTensorValue(indices, csr.data, csr.shape)
+def csr_to_stv(csr: List[T]) -> List[T]:
+    indices = np.array(csr.nonzero()).T  # type: ignore
+    return tf.SparseTensorValue(indices, csr.data, csr.shape)  # type: ignore
 
-def to_tfidf(train, dev, test):
+def to_tfidf(train, dev, test):  # type: ignore
     data = train + dev + test
     comments = [c.comment for c in data]
-    sparse_counts = TfidfVectorizer(norm='l2', ngram_range=(1,5), max_features=30000, dtype=np.float32).fit_transform(comments)  # lowercase = False?
+    sparse_counts = TfidfVectorizer(norm="l2", ngram_range=(1,5), max_features=30000, dtype=np.float32).fit_transform(comments)  # lowercase = False?
 
     train_counts = sparse_counts[:len(train)]
     dev_counts = sparse_counts[len(train):len(train) + len(dev)]
@@ -205,7 +275,7 @@ def to_tfidf(train, dev, test):
 
     return train_counts, train_labels, dev_counts, dev_labels, test_counts, test_labels
 
-def save_tfidf(train_counts, train_labels, dev_counts, dev_labels, test_counts, test_labels, path=""):
+def save_tfidf(train_counts, train_labels, dev_counts, dev_labels, test_counts, test_labels, path=""):  # type: ignore
     save_csr(os.path.join(path, "train_counts"), train_counts)
     np.save(os.path.join(path, "train_labels"), train_labels)
     save_csr(os.path.join(path, "dev_counts"), dev_counts)
@@ -213,7 +283,7 @@ def save_tfidf(train_counts, train_labels, dev_counts, dev_labels, test_counts, 
     save_csr(os.path.join(path, "test_counts"), test_counts)
     np.save(os.path.join(path, "test_labels"), test_labels)
 
-def load_tfidf(path=""):
+def load_tfidf(path=""):  # type: ignore
     return load_csr(os.path.join(path, "train_counts.npz")),\
            np.load(os.path.join(path, "train_labels.npy")),\
            load_csr(os.path.join(path, "dev_counts.npz")),\
@@ -221,17 +291,12 @@ def load_tfidf(path=""):
            load_csr(os.path.join(path, "test_counts.npz")),\
            np.load(os.path.join(path, "test_labels.npy"))
 
-def save_csr(filename, array):
-    np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr,
-             shape=array.shape)
+def save_csr(filename: str, array: List[T]) -> None:  # type: ignore
+    np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr,  # type: ignore
+             shape=array.shape)  # type: ignore
 
-def load_csr(filename):
+def load_csr(filename: str) -> List[List[float]]:  # type: ignore
     loader = np.load(filename)
-    return csr_matrix((loader['data'], loader['indices'], loader['indptr']),
-                      shape=loader['shape'])
+    return csr_matrix((loader["data"], loader["indices"], loader["indptr"]),
+                      shape=loader["shape"])
 
-def show_roc_curve(labels, probs):
-    fpr, tpr, thresholds = roc_curve(labels, probs)
-    plt.plot(fpr, tpr)
-    plt.show()
-    return fpr, tpr, thresholds
