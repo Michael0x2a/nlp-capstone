93edc9621de6995a1766d230b2000263254fdb09
Michael Lee <michael.lee.0x2a@gmail.com>
2017-05-15 19:12:11 +0000

    Whoops, roc score needs macro

--- abuse/models/model.py
+++ abuse/models/model.py
@@ -64,7 +64,7 @@ class BinaryClassificationMetrics:
         self.precision = metrics.precision_score(y_expected, y_predicted, average='binary')
         self.recall = metrics.recall_score(y_expected, y_predicted, average='binary')
         self.f1 = metrics.f1_score(y_expected, y_predicted, average='binary')
-        self.roc_auc = metrics.roc_auc_score(y_expected_hot, y_predicted_prob, average='binary')
+        self.roc_auc = metrics.roc_auc_score(y_expected_hot, y_predicted_prob, average='macro')
         self.spearman = stats.spearmanr(y_expected, y_predicted).correlation
         self.confusion_matrix = metrics.confusion_matrix(y_expected, y_predicted)
         self.fpr, self.tpr, self.thr = metrics.roc_curve(
